88e7096492396a434bcd19e4fc3b9255086a37e2
Fenrir422 <36697498+Fenrir422@users.noreply.github.com>
2021-07-14 23:12:08 +0300

    Updates

---
+++ precache-manifest.5a53f560fc494eaa348b80233beaaa4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86608603a7a90b50afd536deebc905d7",
+    "url": "/Plum/index.html"
+  },
+  {
+    "revision": "99590c56bf778d97f99c",
+    "url": "/Plum/static/css/main.7a9b47d9.chunk.css"
+  },
+  {
+    "revision": "081c53701713e2cc506a",
+    "url": "/Plum/static/js/2.c4051c68.chunk.js"
+  },
+  {
+    "revision": "99590c56bf778d97f99c",
+    "url": "/Plum/static/js/main.2f5ec474.chunk.js"
+  },
+  {
+    "revision": "fe56025912d85c7f7256",
+    "url": "/Plum/static/js/runtime-main.683548be.js"
+  },
+  {
+    "revision": "90863081d318145c82f02817d131c22d",
+    "url": "/Plum/static/media/comment.90863081.png"
+  },
+  {
+    "revision": "69eedac73c0e24bdfd2cc0f929b472e9",
+    "url": "/Plum/static/media/page_examp.69eedac7.png"
+  },
+  {
+    "revision": "4ff1f710e2031c8877fa8bb316e4cf3d",
+    "url": "/Plum/static/media/settings.4ff1f710.png"
+  },
+  {
+    "revision": "e557c77aebaca2b542a5cd3df2808d41",
+    "url": "/Plum/static/media/uroboros.e557c77a.png"
+  },
+  {
+    "revision": "25a553fcbbdc9aad7ac13d70fa3a615f",
+    "url": "/Plum/static/media/usergal.25a553fc.png"
+  }
+]);
